be266d1ad09059859eaa95b30e9c8f26c9cb0692
abhijeet4725 <abhijeetcs4725@gmail.com>
2023-11-26 16:43:34 +0530

    Calculator With Loop

--- main.py
+++ main.py
@@ -18,16 +18,20 @@ class Calculator:
     def Division(self):
         total = self.first_num / self.second_num
         return total
+decision = str(input("Enter Y if you want to use calculator else N:")).upper()
 
-Number1 = float(input("Enter your first number:"))
-Number2 = float(input("Enter your first number:"))
-result = Calculator(Number1 , Number2)
-operator = str(input("Enter your operator from (+ , - , * , /):"))
-if operator == "+":
-    print(result.Addition())
-elif operator == "-":
-    print(result.Subtraction())
-elif operator == "*":
-    print(result.Multiplication())
-elif operator == "/":
-    print(result.Division())
+while decision == "Y":
+    Number1 = float(input("Enter your first number:"))
+    Number2 = float(input("Enter your Second number:"))
+    result = Calculator(Number1 , Number2)
+    operator = str(input("Enter your operator from (+ , - , * , /):"))
+
+    if operator == "+":
+        print(result.Addition())
+    elif operator == "-":
+        print(result.Subtraction())
+    elif operator == "*":
+        print(result.Multiplication())
+    elif operator == "/":
+        print(result.Division())
+    decision = str(input("Enter Y if you want to use calculator else N:")).upper()
